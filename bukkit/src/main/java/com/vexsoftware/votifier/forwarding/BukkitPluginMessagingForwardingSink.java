0b450ab40d17e37c7fa38a8714bfacb6b898e759
Spigot188Support <46186641+Spigot188Support@users.noreply.github.com>
2018-12-27 03:13:24 -0500

    Add files via upload

---
+++ bukkit/src/main/java/com/vexsoftware/votifier/forwarding/BukkitPluginMessagingForwardingSink.java
@@ -0,0 +1,53 @@
+package com.vexsoftware.votifier.forwarding;
+
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+import com.vexsoftware.votifier.NuVotifierBukkit;
+import com.vexsoftware.votifier.model.Vote;
+import com.vexsoftware.votifier.util.GsonInst;
+import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+import java.io.CharArrayReader;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+
+/**
+ * Created by Joe Hirschfeld on 10/20/2015.
+ */
+public class BukkitPluginMessagingForwardingSink implements ForwardingVoteSink, PluginMessageListener {
+
+    public BukkitPluginMessagingForwardingSink(Plugin p, String channel, ForwardedVoteListener listener) {
+        Validate.notNull(channel, "Channel cannot be null.");
+        this.channel = channel;
+        Bukkit.getMessenger().registerIncomingPluginChannel(p, channel, this);
+        this.listener = listener;
+        this.p = p;
+    }
+
+    private final Plugin p;
+    private final String channel;
+    private final ForwardedVoteListener listener;
+
+    @Override
+    public void halt() {
+        Bukkit.getMessenger().unregisterIncomingPluginChannel(p, channel, this);
+    }
+
+    @Override
+    public void onPluginMessageReceived(String s, Player player, byte[] bytes) {
+        String message = new String(bytes, StandardCharsets.UTF_8);
+        try (JsonReader reader = new JsonReader(new CharArrayReader(message.toCharArray()))){
+            while (reader.hasNext()) {
+                JsonObject jsonObject = GsonInst.gson.fromJson(reader, JsonObject.class);
+                Vote v = new Vote(jsonObject);
+                listener.onForward(v);
+            }
+        } catch (Exception e) {
+            NuVotifierBukkit.getInstance().getLogger().log(Level.SEVERE, "There was an unknown error when processing a forwarded vote.", e);
+        }
+    }
+}
